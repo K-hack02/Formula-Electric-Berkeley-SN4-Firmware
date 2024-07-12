2e4700565d749d8c1b135565905ac609bcb2185d
Aman Doshi <amandoshi001@gmail.com>
2024-07-11 18:07:54 -0700

    Serial Monitor: update style to pep 8 standard

--- Tools/Serial_Monitor/main.py
+++ Tools/Serial_Monitor/main.py
@@ -1,50 +1,53 @@
-import serial, serial.tools.list_ports
+import serial, serial.tools.list_ports, serial.tools.list_ports_common
 import sys
 
-class Serial_Connection:
+PortType = serial.tools.list_ports_common.ListPortInfo
+
+class SerialConnection:
     BAUDRATE = 115200
     BYTESIZE = serial.SEVENBITS
     PARITY = serial.PARITY_EVEN
     STOPBITS = serial.STOPBITS_ONE
 
     def __init__(self):
-        ports = self.__get_active_ports()
-        self.__display_active_ports(ports)
-        port_str = self.__select_active_port(ports)
-        self.connection = self.__connect(port_str)
+        ports: list[PortType] = self._get_active_ports()
+        self._display_active_ports(ports)
+        selected_port: str = self._select_active_port(ports)
+        self.connection: serial.Serial = self._connect(selected_port)
     
-    def read_data(self):
+    def read_data(self) -> None:
         self.connection.readline()
         message = ""
         while True:
             try:
-                message = self.connection.readline()
+                message: bytes = self.connection.readline()
                 print(message)
             except KeyboardInterrupt:
                 sys.exit(0)
             except:
                 print(message)
 
-    def __connect(self, port_str):
+    def _connect(self, port: str) -> serial.Serial:
         try:
-            serial_connection = serial.Serial(port_str, Serial_Connection.BAUDRATE, Serial_Connection.BYTESIZE, 
-                                              Serial_Connection.PARITY, Serial_Connection.STOPBITS)
+            serial_connection: serial.Serial = serial.Serial(port,
+                                                             SerialConnection.BAUDRATE, SerialConnection.BYTESIZE,
+                                                             SerialConnection.PARITY, SerialConnection.STOPBITS)
             return serial_connection
         except serial.SerialException as err:
             raise Exception("Cannot connect to serial port.")
     
-    def __display_active_ports(self, ports):
-        ports_str = [p[0] for p in ports]
+    def _display_active_ports(self, ports: list[PortType]) -> None:
+        ports_str: list[str] = [p[0] for p in ports]
         print(f"Active ports: {ports_str}")
 
-    def __get_active_ports(self):
-        ports = serial.tools.list_ports.comports()
+    def _get_active_ports(self) -> list[PortType]:
+        ports: list[PortType] = serial.tools.list_ports.comports()
         if len(ports) == 0:
             raise Exception("No active ports.")
         return ports
 
-    def __select_active_port(self, ports):
-        port_index = -1
+    def _select_active_port(self, ports: list[PortType]) -> str:
+        port_index: int = -1
         while not 1 <= port_index <= len(ports):
             try:
                 port_index = int(input(f"Choose port [1, {len(ports)}]: "))
@@ -52,8 +55,8 @@ class Serial_Connection:
                 pass
         return ports[port_index - 1][0]
 
-def main():
-    sc = Serial_Connection()
+def main() -> None:
+    sc: SerialConnection = SerialConnection()
     sc.read_data()
 
 if __name__ == "__main__":
